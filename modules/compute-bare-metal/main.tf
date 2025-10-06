53457430c3246a6243ae7d4e2fe6fd16c9157796
jimb0 <jimccann@redhat.com>
2025-10-06 15:11:57 -0400

    m2 issue

--- modules/compute-bare-metal/main.tf
+++ modules/compute-bare-metal/main.tf
@@ -66,10 +66,10 @@ resource "ibm_compute_bare_metal" "quote_bms_id" {
   network_speed = var.network_speed
   quote_id = var.quote_id
   storageGroups {
-    array_size 480
-    array_type_id 2
-    disk_controller_index 1
-    hard_drive_category_codes [ secondary_pcie_slot0, secondary_pcie_slot1 ]
+    array_size = 480
+    array_type_id = 2
+    disk_controller_index = 1
+    hard_drive_category_codes = [ secondary_pcie_slot0, secondary_pcie_slot1 ]
     }
   }
 
